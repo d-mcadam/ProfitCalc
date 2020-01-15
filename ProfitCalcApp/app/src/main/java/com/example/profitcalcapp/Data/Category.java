811cac07ec6b8b099ff75ec3b2ba44854273b67b
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-15 22:07:59 +0000

    Update Category.java
    Initialised class with two parameters, title and list for data entries, created basic constructors for this class

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Data/Category.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Data/Category.java
@@ -1,4 +1,36 @@
 package com.example.profitcalcapp.Data;
 
+import java.util.ArrayList;
+
 public class Category {
+
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+    private String title = "";
+    private ArrayList<DataEntry> entries = new ArrayList<>();
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    public Category(){}
+
+    public Category(String title){
+        this.title = title;
+    }
+
+    public Category(String title, ArrayList<DataEntry> entries){
+        this.title = title;
+        this.entries = entries;
+    }
+    //</editor-fold>
+
+    public Integer EntryCount(){ return this.entries.size(); }
+
+    public Integer TotalProfit(){
+        Integer r = 0;
+
+        for (DataEntry entry :
+                entries) {
+
+        }
+    }
+
 }
