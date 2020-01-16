3a167eff1efb7be3f0055b62be6005962019d006
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-16 14:59:21 +0000

    Update MainActivity.java
    Initialised main activity with an init() method, checking for, loading and/or initialising storage class data where appropriate.

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/MainActivity.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/MainActivity.java
@@ -2,15 +2,46 @@ package com.example.profitcalcapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.profitcalcapp.Data.Storage;
 import com.example.profitcalcapp.R;
+import com.example.profitcalcapp.Utilities.AppDataStorage;
+
+import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Storage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    public void init(){
+
+        Intent intent = getIntent();
+        storage = (Storage) intent.getSerializableExtra(STORAGE_CLASS_DATA);
+
+        if (storage == null){
+
+            Toast.makeText(getApplicationContext(), "Storage was null, loading data file...", Toast.LENGTH_LONG).show();
+            new AppDataStorage(this, storage).execute(true);
+
+            if (storage == null){
+
+                Toast.makeText(getApplicationContext(), "Storage was empty, initialising...", Toast.LENGTH_SHORT).show();
+                storage = new Storage();
+
+            }
+
+        }
+
+    }
+
 }
