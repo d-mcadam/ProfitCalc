19df8d6cf5c2775a425bb8917f13178a948583ac
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-16 12:10:43 +0000

    Update DataTypes.java
    Removed Aura IntDef

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/DataTypes.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/DataTypes.java
@@ -7,14 +7,4 @@ import java.lang.annotation.RetentionPolicy;
 
 public class DataTypes {
 
-    @IntDef({Auras.None, Auras.Vampyrism, Auras.Penance, Auras.Equilibrium, Auras.Sharpshooter})
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface Auras{
-        int None = 0;
-        int Vampyrism = 1;
-        int Penance = 2;
-        int Equilibrium = 3;
-        int Sharpshooter = 4;
-    }
-
 }
