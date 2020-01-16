7fd714f739c065934478beb56b94b9978dfd46f0
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-16 11:55:37 +0000

    Update BooleanString.java
    Defined custom boolean string objects

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/BooleanString.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/BooleanString.java
@@ -1,4 +1,18 @@
 package com.example.profitcalcapp.Utilities;
 
 public class BooleanString {
+
+    private final String msg;
+    private final boolean result;
+
+    public BooleanString(){
+        this.msg = "success";
+        this.result = true;
+    }
+
+    public BooleanString(String msg){
+        this.msg = msg;
+        this.result = false;
+    }
+
 }
