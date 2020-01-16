491f3e595270c8bf530582cd8741615a21977ab9
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-16 12:11:07 +0000

    Create Aura.java
    Created Aura class that can be used to dynamically add and remove items to the system.

---
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Data/Aura.java
@@ -0,0 +1,15 @@
+package com.example.profitcalcapp.Data;
+
+public class Aura {
+
+    private String title = "";
+
+    public Aura(){}
+
+    public Aura(String title){ this.title = title; }
+
+    public String getTitle(){ return this.title; }
+
+    public void setTitle(String title){ this.title = title; }
+
+}
