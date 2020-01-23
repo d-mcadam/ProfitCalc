7fc3ac2fa9ca50dfe13525404391551c9bb0f8a4
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-23 11:17:05 +0000

    Modified async code
    Correctly saving and loading data between instances. The async task method will now 'hang' and wait for a response before proceeding with execution (ensures data has been saved or loaded before moving on to the next activity or action).

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/Commands.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/Commands.java
@@ -2,15 +2,18 @@ package com.example.profitcalcapp.Utilities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.example.profitcalcapp.Data.Storage;
+
+import java.util.concurrent.ExecutionException;
 
 import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
 
 public class Commands {
 
     /**
-     * Starts any activity
+     * Starts any activity.
      * @param oldActivity   The current activity (you're probably coding in it).
      * @param storage       The storage class (Passed between activities and class because idk what/how else to do).
      * @param newActivity   The new activity you are going to load. You need the Activities CLASS, so name the activity and add '.class'.
@@ -25,14 +28,20 @@ public class Commands {
     /**
      * Saves all app data and Starts any activity.
      * <p>
-     *     This method will initia
+     *     This method will initially save the app data, and the new activity will load from the async task.
      * </p>
      * @param oldActivity   The current activity (you're probably coding in it).
      * @param storage       The storage class (Passed between activities and class because idk what/how else to do).
      * @param newActivity   The new activity you are going to load. You need the Activities CLASS, so name the activity and add '.class'.
      */
     public void SaveAndStartActivity(Activity oldActivity, Storage storage, Class newActivity){
-        new AppDataStorage(oldActivity, storage, newActivity).execute();
+        try {
+            new AppDataStorage(oldActivity, storage).execute().get();
+            StartActivity(oldActivity, storage, newActivity);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            Toast.makeText(oldActivity.getApplicationContext(), "An error occurred attempting to save the data.", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
