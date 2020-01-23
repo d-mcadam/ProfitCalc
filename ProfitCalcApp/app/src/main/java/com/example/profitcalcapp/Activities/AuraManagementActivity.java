6241b02f82a157b1de66fc0387ed7faaf5d06980
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-23 14:05:47 +0000

    Created Aura Management activity
    With accompanying xml file and updated android manifest to denote the parent of this activity as the SettingsActivity

---
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/AuraManagementActivity.java
@@ -0,0 +1,16 @@
+package com.example.profitcalcapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.profitcalcapp.R;
+
+public class AuraManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aura_management);
+    }
+}
