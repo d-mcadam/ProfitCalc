3cc6719eff93fcd35e696368b3942606fe89a7c5
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-23 14:27:33 +0000

    Update AuraManagementActivity.java
    Initialising the class with Storage class through intent, Overridden code for default back-button operation.

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/AuraManagementActivity.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/AuraManagementActivity.java
@@ -2,15 +2,66 @@ package com.example.profitcalcapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.profitcalcapp.Data.Storage;
 import com.example.profitcalcapp.R;
+import com.example.profitcalcapp.Utilities.Commands;
+
+import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
 
 public class AuraManagementActivity extends AppCompatActivity {
 
+    //<editor-fold defaultstate="collapsed" desc="Data Classes">
+    private Storage storage;
+    private final Commands cmds = new Commands();
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Overridden Activity operations">
+
+    //<editor-fold defaultstate="collapsed" desc="The provided button operation">
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 16908332://this is the ID code for a generic "Back" button that's provided by the api
+                cmds.StartActivity(this, storage, SettingsActivity.class);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="The default create method">
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aura_management);
+        init();
     }
+    //</editor-fold>
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Activity initialisation">
+    private void init() {
+
+        //<editor-fold defaultstate="collapsed" desc="Get Storage from Intent">
+        Intent intent = getIntent();
+        storage = (Storage) intent.getSerializableExtra(STORAGE_CLASS_DATA);
+        if (storage == null){
+            Toast.makeText(this, "Error loading storage in AuraManagementActivity.class", Toast.LENGTH_LONG).show();
+            cmds.StartActivity(this, storage, SettingsActivity.class);
+            return;
+        }
+        //</editor-fold>
+
+    }
+    //</editor-fold>
+
 }
