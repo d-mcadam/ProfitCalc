7215d8a44a33ec3023b1d92daa03283e5bae18b6
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-24 20:47:29 +0000

    Created CreateCategoryActivity
    and accompanying xml file including android manifest update. Did quite a few basic 'shell' bits of code copied from an existing similar activity

---
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateCategoryActivity.java
@@ -0,0 +1,96 @@
+package com.example.profitcalcapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.profitcalcapp.Data.Storage;
+import com.example.profitcalcapp.R;
+import com.example.profitcalcapp.Utilities.Commands;
+
+import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
+
+public class CreateCategoryActivity extends AppCompatActivity {
+
+    //<editor-fold defaultstate="collapsed" desc="Data Classes">
+    private Storage storage;
+    private final Commands cmds = new Commands();
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Activity Views">
+    private EditText fieldCategoryTitle;
+    //</editor-fold>
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 16908332://this is the ID code for a generic "Back" button that's provided by the api
+                if (UnsavedData()){
+
+                }else {
+                    cmds.StartActivity(this, storage, CategoryActivity.class);
+                }
+                break;
+        }
+        return true;
+    }
+    private boolean UnsavedData(){
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_category);
+        init();
+    }
+
+    private void init(){
+
+        //<editor-fold defaultstate="collapsed" desc="Get Storage from Intent">
+        Intent intent = getIntent();
+        storage = (Storage) intent.getSerializableExtra(STORAGE_CLASS_DATA);
+        if (storage == null){
+            Toast.makeText(this, "Error loading storage in CreateCategoryActivity.class", Toast.LENGTH_LONG).show();
+            cmds.StartActivity(this, storage, MainActivity.class);
+            return;
+        }
+        //</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="Initialise title field">
+        fieldCategoryTitle = findViewById(R.id.editTextCategoryTitle);
+        fieldCategoryTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        //</editor-fold>
+
+    }
+
+    private void CheckSaveEligibility(){
+        boolean titleExists = !fieldCategoryTitle.getText().toString().trim().equals("");
+        if (!titleExists)
+            fieldCategoryTitle.setError("Title cannot be empty");
+
+        
+    }
+
+}
