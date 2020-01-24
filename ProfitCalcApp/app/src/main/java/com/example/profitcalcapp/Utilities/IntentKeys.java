3dad6151045bd9803151005e3a92d4cf5f3d0473
d-mcadam <dylan@dylanmcadam.co.uk>
2020-01-24 15:52:32 +0000

    Update IntentKeys.java
    added intent keys

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/IntentKeys.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/IntentKeys.java
@@ -2,8 +2,16 @@ package com.example.profitcalcapp.Utilities;
 
 public class IntentKeys {
 
+    //for the entirety of the app data
     public static final String APP_STORAGE_DATA = "APP_STORAGE_DATA";
 
+    //for parsing storage class only
     public static final String STORAGE_CLASS_DATA = "STORAGE_CLASS_DATA";
+
+    //for editing an aura
+    public static final String EDIT_AURA_KEY = "EDIT_AURA_KEY";
+
+    //for parsing string values
+    public static final String STRING_PASS_KEY = "STRING_PASS_KEY";
 
 }
