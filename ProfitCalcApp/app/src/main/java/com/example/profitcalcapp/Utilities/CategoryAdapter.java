fbb6490afce4457ed8ae987ad8a77e5e54f34b5c
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-24 19:10:14 +0000

    Create CategoryAdapter.java
    Base category recycler view adapter

---
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/CategoryAdapter.java
@@ -0,0 +1,126 @@
+package com.example.profitcalcapp.Utilities;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.profitcalcapp.Data.Aura;
+import com.example.profitcalcapp.Data.Storage;
+import com.example.profitcalcapp.R;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private final Storage storage;
+    private final Commands cmds = new Commands();
+
+    private Context context;
+    private List<Aura> items;
+
+    public int focusedPosition = -1;
+
+    public CategoryAdapter(Context context, List<Aura> items, Storage storage){
+        this.context = context;
+        this.items = items;
+        this.storage = storage;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(context).inflate(R.layout.custom_row_category, parent, false);
+        row.getLayoutParams().height = parent.getMeasuredHeight() / 12;
+        return new Item(row);
+    }
+
+    protected void ResetViewHolder(RecyclerView.ViewHolder holder){
+        holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.originalWhite, null));
+        ((Item) holder).view.setVisibility(View.INVISIBLE);
+        ((Item) holder).view.setClickable(false);
+        ((Item) holder).edit.setVisibility(View.INVISIBLE);
+        ((Item) holder).edit.setClickable(false);
+        ((Item) holder).delete.setVisibility(View.INVISIBLE);
+        ((Item) holder).delete.setClickable(false);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+
+        final int selectedColour = context.getResources().getColor(R.color.selectedGrey, null);
+        final Aura item = items.get(position);
+        ((Item)holder).title.setText(item.getTitle());
+
+        if (focusedPosition == position){
+            if (((ColorDrawable)holder.itemView.getBackground()).getColor() != selectedColour){
+                holder.itemView.setBackgroundColor(selectedColour);
+                ((Item) holder).view.setVisibility(View.VISIBLE);
+                ((Item) holder).view.setClickable(true);
+                ((Item) holder).edit.setVisibility(View.VISIBLE);
+                ((Item) holder).edit.setClickable(true);
+                ((Item) holder).delete.setVisibility(View.VISIBLE);
+                ((Item) holder).delete.setClickable(true);
+            }else{
+                ResetViewHolder(holder);
+            }
+        }else{
+            ResetViewHolder(holder);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                focusedPosition = position;
+                notifyDataSetChanged();
+            }
+        });
+
+        ((Item) holder).view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        ((Item) holder).edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        ((Item) holder).delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() { return items.size(); }
+
+    public class Item extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView brief;
+        ImageButton view;
+        ImageButton edit;
+        ImageButton delete;
+
+        public Item(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.rowTitleCategory);
+            brief = itemView.findViewById(R.id.rowBriefCategory);
+            view = itemView.findViewById(R.id.rowButtonViewCategory);
+            edit = itemView.findViewById(R.id.rowButtonEditCategory);
+            delete = itemView.findViewById(R.id.rowButtonDeleteCategory);
+        }
+    }
+
+}
