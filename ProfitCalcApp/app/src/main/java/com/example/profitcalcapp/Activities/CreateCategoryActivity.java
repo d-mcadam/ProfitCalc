88ed848bb2b196dd0680fed2e58b4ed8363a4d14
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-24 21:02:04 +0000

    Update CreateCategoryActivity.java
    Updating save button eligibility and creating warning dialog for unsaved changes

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateCategoryActivity.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateCategoryActivity.java
@@ -1,15 +1,20 @@
 package com.example.profitcalcapp.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.profitcalcapp.Data.Category;
 import com.example.profitcalcapp.Data.Storage;
 import com.example.profitcalcapp.R;
 import com.example.profitcalcapp.Utilities.Commands;
@@ -25,6 +30,11 @@ public class CreateCategoryActivity extends AppCompatActivity {
 
     //<editor-fold defaultstate="collapsed" desc="Activity Views">
     private EditText fieldCategoryTitle;
+    private Button buttonSave;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+    private final Activity thisActivity = this;
     //</editor-fold>
 
     @Override
@@ -32,7 +42,22 @@ public class CreateCategoryActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case 16908332://this is the ID code for a generic "Back" button that's provided by the api
                 if (UnsavedData()){
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(this);
 
+                    dialog.setCancelable(true);
+                    dialog.setTitle("Unsaved data");
+                    dialog.setMessage("You will lose any unsaved data, do you want to continue?");
+
+                    dialog.setNegativeButton("Go Back", null);
+
+                    dialog.setPositiveButton("Discard Changes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            cmds.StartActivity(thisActivity, storage, CategoryActivity.class);
+                        }
+                    });
+
+                    dialog.create().show();
                 }else {
                     cmds.StartActivity(this, storage, CategoryActivity.class);
                 }
@@ -41,7 +66,8 @@ public class CreateCategoryActivity extends AppCompatActivity {
         return true;
     }
     private boolean UnsavedData(){
-        return false;
+        String title = fieldCategoryTitle.getText().toString().trim();
+        return !title.equals("");
     }
 
     @Override
@@ -63,26 +89,36 @@ public class CreateCategoryActivity extends AppCompatActivity {
         }
         //</editor-fold>
 
+        //<editor-fold defaultstate="collapsed" desc="Reference activity Views">
+        buttonSave = findViewById(R.id.buttonSaveCategory);
+        //</editor-fold>
+
         //<editor-fold defaultstate="collapsed" desc="Initialise title field">
         fieldCategoryTitle = findViewById(R.id.editTextCategoryTitle);
         fieldCategoryTitle.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { CheckSaveEligibility(); }
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { CheckSaveEligibility(); }
             @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
+            public void afterTextChanged(Editable editable) { CheckSaveEligibility(); }
         });
         //</editor-fold>
 
+        CheckSaveEligibility();
+
+    }
+
+    private boolean DuplicateTitle(){
+
+        for (Category category : storage.getCategories()){
+            if (category.getTitle().toLowerCase().equals(fieldCategoryTitle.getText().toString().toLowerCase().trim())){
+                fieldCategoryTitle.setError("Category name already exists.");
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void CheckSaveEligibility(){
@@ -90,7 +126,13 @@ public class CreateCategoryActivity extends AppCompatActivity {
         if (!titleExists)
             fieldCategoryTitle.setError("Title cannot be empty");
 
-        
+        boolean duplicateTitle = DuplicateTitle();
+        boolean eligible = titleExists && !duplicateTitle;
+        int colour = eligible ? getResources().getColor(R.color.pureGreen, null) : getResources().getColor(R.color.greyedOut, null);
+
+        buttonSave.setClickable(eligible);
+        buttonSave.setBackgroundColor(colour);
+        buttonSave.setTooltipText(eligible ? "Save Aura" : !titleExists ? "Need a title" : "Duplicate Title");
     }
 
 }
