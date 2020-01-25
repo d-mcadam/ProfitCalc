82408189f40a57ff86f820aca0eff1c34f1b7a8b
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-25 09:11:33 +0000

    Generate a CreateDataEntryActivity
    including manifest reference and set parent activity and accompanying xml document

---
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateDataEntryActivity.java
@@ -0,0 +1,21 @@
+package com.example.profitcalcapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.profitcalcapp.R;
+
+public class CreateDataEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_data_entry);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
