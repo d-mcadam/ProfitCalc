bb25078d90e3f15b3ee4cde2f24ed1cdb1a4cc7e
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-25 09:41:53 +0000

    Update CreateDataEntryActivity.java
    Working basics with back button, safely passing data between activities, and initial but untested unsaved data check operation and dialog box generation

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateDataEntryActivity.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateDataEntryActivity.java
@@ -1,12 +1,63 @@
 package com.example.profitcalcapp.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.profitcalcapp.Data.Storage;
 import com.example.profitcalcapp.R;
+import com.example.profitcalcapp.Utilities.Commands;
+
+import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
 
 public class CreateDataEntryActivity extends AppCompatActivity {
+
+    //<editor-fold defaultstate="collapsed" desc="Data Classes">
+    private Storage storage;
+    private final Commands cmds = new Commands();
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+    private final Activity thisActivity = this;
+    //</editor-fold>
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 16908332://this is the ID code for a generic "Back" button that's provided by the api
+                if (UnsavedData()){
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+                    dialog.setCancelable(true);
+                    dialog.setTitle("Unsaved data");
+                    dialog.setMessage("You will lose any unsaved data, do you want to continue?");
+
+                    dialog.setNegativeButton("Go Back", null);
+
+                    dialog.setPositiveButton("Discard Changes", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            cmds.StartActivity(thisActivity, storage, CreateCategoryActivity.class);
+                        }
+                    });
+                }else{
+                    cmds.StartActivity(this, storage, CreateCategoryActivity.class);
+                }
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+    private boolean UnsavedData(){
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +67,16 @@ public class CreateDataEntryActivity extends AppCompatActivity {
     }
 
     private void init(){
+
+        //<editor-fold defaultstate="collapsed" desc="Get Storage from Intent">
+        Intent intent = getIntent();
+        storage = (Storage) intent.getSerializableExtra(STORAGE_CLASS_DATA);
+        if (storage == null){
+            Toast.makeText(this, "Error loading storage in CreateDataEntryActivity.class", Toast.LENGTH_LONG).show();
+            cmds.StartActivity(this, storage, CreateCategoryActivity.class);
+            return;
+        }
+        //</editor-fold>
 
     }
 }
