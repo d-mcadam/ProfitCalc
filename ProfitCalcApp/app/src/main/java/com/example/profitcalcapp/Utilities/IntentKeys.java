b23d120c7d93389274eaa6795bcd24135ee401da
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-25 09:28:33 +0000

    Update IntentKeys.java
    Added an intent key to pass collection list between the CreateCategory and DataEntry activity

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/IntentKeys.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Utilities/IntentKeys.java
@@ -5,13 +5,16 @@ public class IntentKeys {
     //for the entirety of the app data
     public static final String APP_STORAGE_DATA = "APP_STORAGE_DATA";
 
-    //for parsing storage class only
+    //for passing storage class only
     public static final String STORAGE_CLASS_DATA = "STORAGE_CLASS_DATA";
 
     //for editing an aura
     public static final String EDIT_AURA_KEY = "EDIT_AURA_KEY";
 
-    //for parsing string values
+    //for passing string values
     public static final String STRING_PASS_KEY = "STRING_PASS_KEY";
+
+    //for passing collection list from create category to create entry activity
+    public static final String COLLECTION_PASS_KEY = "COLLECTION_PASS_KEY";
 
 }
