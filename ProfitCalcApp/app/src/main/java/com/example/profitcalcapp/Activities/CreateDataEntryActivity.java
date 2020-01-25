6661a9ba8e3c2e5416e2f3059f9b52709368be24
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-01-25 11:35:22 +0000

    Update CreateDataEntryActivity.java
    Added framework to create a working spinner with ability to select auras

--- ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateDataEntryActivity.java
+++ ProfitCalcApp/app/src/main/java/com/example/profitcalcapp/Activities/CreateDataEntryActivity.java
@@ -8,11 +8,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.profitcalcapp.Data.Aura;
 import com.example.profitcalcapp.Data.Storage;
 import com.example.profitcalcapp.R;
 import com.example.profitcalcapp.Utilities.Commands;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.profitcalcapp.Utilities.IntentKeys.STORAGE_CLASS_DATA;
 
@@ -21,6 +27,10 @@ public class CreateDataEntryActivity extends AppCompatActivity {
     //<editor-fold defaultstate="collapsed" desc="Data Classes">
     private Storage storage;
     private final Commands cmds = new Commands();
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Activity Views">
+    private Spinner auraSpinner;
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Variables">
@@ -76,6 +86,24 @@ public class CreateDataEntryActivity extends AppCompatActivity {
             cmds.StartActivity(this, storage, CreateCategoryActivity.class);
             return;
         }
+        //</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="Initialise spinner">
+        //id / reference the view
+        auraSpinner = findViewById(R.id.spinnerAuraSelector);
+        //create a list to hold the strings
+        final List<String> stringList = new ArrayList<>();
+        //add a default / initial value
+        stringList.add("None");
+        //add the remaining auras held in storage
+        for (Aura aura : storage.getAuras())
+            stringList.add(aura.getTitle());
+        //create an adapter with the list of strings created above
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, stringList);
+        //set the drop down view layout
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        //set the adapter to the view spinner object
+        auraSpinner.setAdapter(arrayAdapter);
         //</editor-fold>
 
     }
